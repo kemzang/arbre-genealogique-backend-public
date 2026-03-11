7949c71708348ec3c696ee20d0f4d1a3ca6b57f7
kemzang <kemzangbryan@gmail.com>
2026-03-11 15:38:04 +0100

    Add PATCH method to CORS allowed methods

--- proxy.ts
+++ proxy.ts
@@ -29,7 +29,7 @@ export function proxy(request: NextRequest) {
       status: 200,
       headers: {
         'Access-Control-Allow-Origin': isAllowed ? origin : allowedOrigins[0],
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
     })
@@ -44,7 +44,7 @@ export function proxy(request: NextRequest) {
     response.headers.set('Access-Control-Allow-Origin', allowedOrigins[0])
   }
   
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
   return response
